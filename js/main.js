ae9e3c5e3bcac7edf6b2e04ea2c0ee8fca06660d
andre <61792917+luuxis@users.noreply.github.com>
2024-06-18 12:38:55 +0200

    add list projet

---
+++ js/main.js
@@ -0,0 +1,46 @@
+class GitHubProjects {
+    constructor(user) {
+        this.user = user;
+        this.projects = [];
+        this.loadProjects();
+    }
+
+    async fetchData() {
+        const response = await fetch(`https://api.github.com/users/${this.user}/repos`);
+        const data = await response.json();
+        return data;
+    }
+
+    async loadProjects() {
+        this.projects = await this.fetchData();
+        this.sortProjects();
+    }
+
+    sortProjects() {
+        this.projects.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+    }
+
+    getProjects() {
+        return this.projects;
+    }
+}
+
+(async () => {
+    const gitHubProjects = new GitHubProjects('luuxis');
+    await gitHubProjects.loadProjects();
+
+    for (const project of gitHubProjects.getProjects()) {
+        const projectElement = document.createElement('div');
+        projectElement.innerHTML = `
+            <h3>${project.name}</h2>
+            <p>${project.description || 'No description'}</p>
+            <p>Language: ${project.language}</p>
+            <p>created at: ${new Date(project.created_at).toLocaleDateString()}</p>
+            <a href="${project.html_url}" target="_blank">View on GitHub</a>
+        `;
+
+        projectElement.classList.add('project');
+
+        document.querySelector('.projects .container ').appendChild(projectElement);
+    }
+})();
